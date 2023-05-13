6c293e0399bb6d68a5b45bf730758929008096c7
Irem Ecem Yelkanat <iremecemyelkanat@gmail.com>
2023-05-13 20:54:33 +0300

    chore(app): add middleware related libraries

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,5 +1,11 @@
 import {Application, json, urlencoded, Response, Request, NextFunction} from 'express';
 import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cookieSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
+import 'express-async-errors';
 
 export class BuzzverseServer {
     private app: Application;
