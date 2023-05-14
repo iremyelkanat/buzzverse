d85254b2aec7b8879bdd17e0ad4c15dfcd4cf03c
iremyelkanat <iremecemyelkanat@gmail.com>
2023-05-14 21:58:56 +0300

    feat(app): setup socketio conection

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,7 @@ class Config {
     public SECRET_KEY_ONE: string | undefined;
     public SECRET_KEY_TWO: string | undefined;
     public CLIENT_URL: string | undefined;
+    public REDIS_HOST: string | undefined;
 
     private readonly DEFAULT_DATABASE_URL =
         'mongodb://localhost:27017/buzzverse-backend';
@@ -21,10 +22,10 @@ class Config {
         this.SECRET_KEY_ONE = process.env.SECRET_KEY_ONE || '';
         this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
         this.CLIENT_URL = process.env.CLIENT_URL || '';
+        this.REDIS_HOST = process.env.REDIS_HOST || '';
     }
 
     public validateConfig(): void {
-        console.log(this);
         for (const [key, value] of Object.entries(this)) {
             if (value === undefined) {
                 throw new Error(`Configuration ${key} is undefined.`);
